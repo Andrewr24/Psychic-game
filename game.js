032b71725b20b4d9971c96f35fa0c6cd32ebbbf6
Andrewr24 <reyesandrew98@yahoo.com>
2019-11-09 18:32:31 -0500

    javascript implemented (COMPLETE)

--- game.js
+++ game.js
@@ -1,37 +1,65 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var wins= 0;
-var losses= 0;
-var ties= 0;
-var numGuesses= 9;
-var guessChoices=[];
-var userText = document.getElementById("user-text");
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  , randomLetter = ""
+  , winCount = 0
+  , lossCount = 0
+  , guessesLeft = 0
+  , guessedLetters = []
+  , defaultGuessesLeft = 9;
 
-document.onkeyup = function(event) {
-    userText.textContent = event.key;
-    var userGuess= event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+window.addEventListener("keypress", onKeyPress, false);
+newGame();
 
-};
-if (options.indexOf(userGuess) > -1) {
-
-    if (userGuess === computerGuess) {
-        wins++;
-        numGuesses = 9;
-        guessChoices = [];
-    }
-
-    if (userGuess != computerGuess) {
-        numGuesses --;
-        guessChoices.push(userGuess);
-    }
-
-    if (numGuesses === 0) {
-
-    numGuesses = 9;
-    losses ++;
-    guessChoices = [];
-
-    
+function newGame() {
+  guessedLetters = [];
+  resetGuesses();
+  generateRandomLetter();
+  console.log(randomLetter); 
+  displayOnScreen("wins", `Wins: ${String(winCount)}`);
+  displayOnScreen("losses", `Losses: ${String(lossCount)}`);
+  displayOnScreen("guessesleft", `Guesses Left: ${String(guessesLeft)}`);
 }
+
+function onKeyPress(key) {
+  letter = key.key.toLowerCase();
+  if (letters.includes(letter, 0) && guessedLetters.includes(letter, 0) === false) {
+    guessedLetters.push(letter);
+    printKeyPressed(letter);
+    guessesLeft--;
+    displayOnScreen("guessesleft", `Guesses Left: ${String(guessesLeft)}`);
+    if (String(letter) == String(randomLetter)) {
+      winCount++;
+      displayOnScreen("wins", `Wins: ${String(winCount)}`);
+      newGame();
+    }
+    if (guessesLeft === 0) {
+      lossCount++;
+      displayOnScreen("losses", `Losses: ${String(lossCount)}`);
+      newGame();
+    }
+  }
+
+
+
+}
+
+function generateRandomLetter() {
+  randomLetter = letters[Math.floor(Math.random() * letters.length)];
+}
+
+function printKeyPressed(letter) {
+  if (guessesLeft == 9) {
+    document.getElementById("guessessofar").innerHTML += letter;
+  } else {
+    document.getElementById("guessessofar").innerHTML += ", " + letter;
+  }
+}
+
+function resetGuesses() {
+  guessesLeft = defaultGuessesLeft;
+  displayOnScreen("guessesleft", `Losses: ${String(guessesLeft)}`);
+  displayOnScreen("guessessofar", `Your Guesses so far: `);
+}
+
+function displayOnScreen(divContainer, content) {
+  document.getElementById(divContainer).innerHTML = content;
 }
